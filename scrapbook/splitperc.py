1cdc64f478c293636a86582c29cea1b5bd192a1b
Mars Saxman <mars@redecho.org>
2018-02-02 23:09:09 -0800

    retain sample rate and stereo separation when splitting audio

--- scrapbook/splitperc.py
+++ scrapbook/splitperc.py
@@ -1,19 +1,26 @@
-# Feature extraction example
-import numpy as np
+#!/usr/bin/env python
+
+import numpy
 import librosa
-import sys
-import nussl
-import os
+import sys, os
 
-# Load the example clip
+# Load the target audio file, then split into left and right.
 path = sys.argv[1]
-y, sr = librosa.load(path)
+signal, samplerate = librosa.load(path, sr=44100, mono=False)
+left = signal[0, :]
+right = signal[1, :]
 
-# Separate harmonics and percussives into two waveforms
-y_harmonic, y_percussive = librosa.effects.hpss(y)
+# Divide left and right into harmonic and percussive components.
+harmonic_left, percussive_left = librosa.effects.hpss(left)
+harmonic_right, percussive_right = librosa.effects.hpss(right)
 
+# Rejoin each left and right channel
+harmonic = numpy.vstack((harmonic_left, harmonic_right))
+percussive = numpy.vstack((percussive_left, percussive_right))
+
+# Write each component back out as a separate audio file
 basepath = os.path.splitext(path)[0]
-librosa.output.write_wav(basepath + "_perc.wav", y_percussive, sr)
-librosa.output.write_wav(basepath + "_harm.wav", y_harmonic, sr)
+librosa.output.write_wav(basepath + "_perc.wav", percussive, samplerate)
+librosa.output.write_wav(basepath + "_harm.wav", harmonic, samplerate)
 
 
