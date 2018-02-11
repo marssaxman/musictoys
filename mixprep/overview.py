eff24103251c18b8d1d298ec121ef18175be67c2
Mars Saxman <mars@redecho.org>
2018-02-11 08:01:12 -0800

    overview now allows view frame to be dragged around

--- mixprep/overview.py
+++ mixprep/overview.py
@@ -25,6 +25,7 @@ class Overview(tk.Canvas):
 		self.bind("<MouseWheel>", self.on_mousewheel)
 		self.bind("<Button-4>", self.on_mousewheel)
 		self.bind("<Button-5>", self.on_mousewheel)
+		self.bind("<ButtonPress-1>", self.on_mousedown)
 		self._draw()
 
 	def on_resize(self, event):
@@ -39,6 +40,25 @@ class Overview(tk.Canvas):
 			# mousewheel down: zoom out
 			self.zoom(7.0 / 8.0, event.x)
 
+	def on_mousedown(self, event):
+		pos = float(event.x) / float(self.winfo_width())
+		begin, end = self.interval
+		if pos >= begin and pos <= end:
+			self._mouse_drag_offset = pos - begin
+			self.bind("<B1-Motion>", self.on_mousemove)
+			self.bind("<ButtonRelease-1>", self.on_mouseup)
+
+	def on_mousemove(self, event):
+		pos = float(event.x) / float(self.winfo_width())
+		begin, end = self.interval
+		length = end - begin
+		begin = pos - self._mouse_drag_offset
+		self.view((begin, begin + length))
+
+	def on_mouseup(self, event):
+		self.unbind("<B1-Motion>")
+		self.unbind("<ButtonRelease-1>")
+
 	def zoom(self, factor, eventx=None):
 		begin, end = self.interval
 		length = end - begin
@@ -46,8 +66,14 @@ class Overview(tk.Canvas):
 		length *= factor
 		if eventx:
 			pos = float(eventx) / float(self.winfo_width())
-		newbegin = max(0, pos - (length / 2.0))
-		newend = min(1, newbegin + length)
+		newbegin = pos - (length / 2.0)
+		newend = newbegin + length
+		self.view((newbegin, newend))
+
+	def view(self, interval):
+		newbegin = max(0, interval[0])
+		newend = min(1, interval[1])
+		begin, end = self.interval
 		if newbegin != begin or newend != end:
 			self.interval = (newbegin, newend)
 			self._draw_interval()
