867a0e6cd01f9abc044ce17dcec346a788d12024
Mars Saxman <mars@redecho.org>
2018-02-11 17:39:13 -0800

    well that was a learning experience

--- mixprep/editview.py
+++ mixprep/editview.py
@@ -5,6 +5,21 @@ matplotlib.use('TkAgg')
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import numpy as np
 
+
+# In the process of building this I have discovered that matplotlib has most
+# of it built in, and this can all be done much, much better.
+# SpanSelector class does interactive range selection:
+#  https://stackoverflow.com/questions/44990722/how-to-select-area-within-a-plot-python-and-extract-the-data-within-the-area
+# How to downsample a large input array in response to xlim_changed:
+#  https://matplotlib.org/examples/event_handling/resample.html
+# Using one plot to zoom in on another:
+#  https://matplotlib.org/examples/event_handling/viewlims.html
+# This might be useful for beat markers:
+#  https://matplotlib.org/examples/event_handling/data_browser.html
+# Signal smoothing example
+#  https://matplotlib.org/gallery/misc/demo_agg_filter.html#sphx-glr-gallery-misc-demo-agg-filter-py
+
+
 class Editview(tk.Frame):
 	def __init__(self, container, signal, **kwargs):
 		tk.Frame.__init__(self, container, **kwargs)
