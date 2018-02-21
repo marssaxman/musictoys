a1539b1aa3dfcf6946f050f3f4e097a30e543390
Mars Saxman <mars@redecho.org>
2018-02-21 05:30:34 -0800

    much faster rendering via better use of numpy and librosa framing

--- mixprep/overview.py
+++ mixprep/overview.py
@@ -1,8 +1,13 @@
 import Tkinter as tk
 import numpy as np
+import librosa
 
-def rms(samples):
-	return np.sqrt(np.mean(samples ** 2))
+
+def _framed_max_rms(signal, step):
+	frames = librosa.util.frame(signal, frame_length=step, hop_length=step)
+	maxmag = np.max(np.abs(frames), axis=0)
+	rms = np.sqrt(np.mean(frames ** 2, axis=0))
+	return maxmag, rms
 
 
 class Overview(tk.Canvas):
@@ -25,7 +30,6 @@ class Overview(tk.Canvas):
 		self.bind("<Button-4>", self.on_mousewheel)
 		self.bind("<Button-5>", self.on_mousewheel)
 		self.bind("<ButtonPress-1>", self.on_mousedown)
-		self._draw()
 
 	def on_resize(self, event):
 		self._draw()
@@ -91,27 +95,23 @@ class Overview(tk.Canvas):
 		self._selection_box_id = None
 		width = self.winfo_width()
 		height = self.winfo_height()
-		samplecount = len(self._signal.mono)
-		left_channel = self._signal.left
-		right_channel = self._signal.right
+		# how many pixels tall should we draw things, and how far offset?
 		vloc = height / 2
 		vscale = height * 3 / 8
+
+		# Chop the signal halves into frames, then get the maxmag and rms value
+		# for each frame, where each frame corresponds to one pixel.
+		framelen = int(len(self._signal.mono) / float(width))
+		left_max, left_rms = _framed_max_rms(self._signal.left, framelen)
+		right_max, right_rms = _framed_max_rms(self._signal.right, framelen)
 		for x in xrange(width):
-			# Get the left and right channel sample bins for this pixel.
-			begin = samplecount * x / width
-			end = samplecount * (x+1) / width
-			left_bin = left_channel[begin:end]
-			right_bin = right_channel[begin:end]
-			# Get the maximum (absolute) and RMS values for these bins.
-			left_max, left_rms = np.max(np.abs(left_bin)), rms(left_bin)
-			right_max, right_rms = np.max(np.abs(right_bin)), rms(right_bin)
 			# Draw the left channel on top and the right channel below, using
 			# dark grey for the max and a lighter grey for RMS.
-			top_pos = vloc - (left_max * vscale)
-			bot_pos = vloc + (right_max * vscale)
+			top_pos = vloc - (left_max[x] * vscale)
+			bot_pos = vloc + (right_max[x] * vscale)
 			self.create_line(x, top_pos, x, bot_pos, fill='#AAAAAA')
-			top_pos = vloc - (left_rms * vscale)
-			bot_pos = vloc + (right_rms * vscale)
+			top_pos = vloc - (left_rms[x] * vscale)
+			bot_pos = vloc + (right_rms[x] * vscale)
 			self.create_line(x, top_pos, x, bot_pos, fill='#CCCCCC')
 		self._draw_view_box()
 		self._draw_selection_box()
