5fc0fd335dcdb1c99397c41aa31a899638b66fbf
Mars Saxman <mars@redecho.org>
2018-05-13 16:30:02 -0700

    restructuring; no longer strictly depends on the soundfile library

--- musictoys/audiofile.py
+++ musictoys/audiofile.py
@@ -1,40 +1,20 @@
 """audiofile reads audio files, combing python libraries and external tools"""
 
+# todo: conditionally import soundfile
+# also try audioread
+
 import collections
 import os
 import subprocess
 import tempfile
-import soundfile
 import struct
 import numpy as np
-
 from collections import namedtuple
+
+# Public API
+
 Info = namedtuple('Info', 'format dtype samplerate channels')
 Clip = namedtuple('Clip', 'data samplerate')
-Format = namedtuple('Format', 'name description extensions')
-_Codec = namedtuple('_Codec', 'formats read write info')
-
-FORMATS = {f.name: f for f in [
-    Format('AAC', "Advanced Audio Coding", {'.aac'}),
-    Format('AIFC', "Compressed AIFF", {'.aifc'}),
-    Format('AIFF', "Audio IFF", {'.aiff'}),
-    Format('AU', "Sun AU", {'.au'}),
-    Format('AVI', "Audio Video Interlaced", {'.avi'}),
-    Format('CAF', "Apple Core Audio Format", {'.caf'}),
-    Format('FLAC', "FLAC", {'.flac'}),
-    Format('M4A', "MPEG-4 Audio Lossless", {'.m4a', '.m4r'}),
-    Format('M4B', "MPEG-4 Audio Books", {'.m4b'}),
-    Format('MP2', "MPEG audio layer 2", {'.mp2'}),
-    Format('MP3', "MPEG audio layer 3", {'.mp3'}),
-    Format('MP4', "MPEG-4 Part 14", {'.mp4'}),
-    Format('OGG', "Ogg", {'.ogg'}),
-    Format('OGA', "Ogg Audio", {'.oga'}),
-    Format('MOGG', "Multitrack Ogg", {'.mogg'}),
-    Format('OPUS', "Ogg Opus", {'.opus'}),
-    Format('WAV', "Waveform Audio", {'.wav'}),
-    Format('WEBM', "WebM", {'.webm'}),
-    Format('WMA', "Windows Media Audio", {'.wma'}),
-]}
 
 
 class FormatError(Exception):
@@ -43,14 +23,102 @@ class FormatError(Exception):
         self.format = format
 
 
-def _execpipe(*args):
-    pipe = subprocess.PIPE
-    proc = subprocess.Popen(args, stdin=pipe, stdout=pipe, stderr=pipe)
-    output, _ = proc.communicate()
-    retcode = proc.returncode
-    if retcode:
-        raise subprocess.CalledProcessError(retcode, args, output=output)
-    return output
+def formats():
+    _init()
+    global _formats
+    return _formats.keys()
+
+
+def extensions():
+    _init()
+    global _extensions
+    return _extensions.keys()
+
+
+def read(file):
+    """Retrieve audio samples from a file.
+
+    Parameters
+    ----------
+    file : str
+        The file to read from.
+
+    Returns
+    -------
+    data : numpy.ndarray
+        If the file is monaural, a one-dimensional array of samples; otherwise
+        a two-dimensional array of [channels, samples].
+
+    samplerate : int
+        The sampling frequency of the audio data in Hz.
+    """
+
+    return Clip(*_dispatch(file).read(file))
+
+
+def write(file, clip):
+    _dispatch(file).write(file, clip)
+
+
+def info(file):
+    return _dispatch(file).info(file)
+
+
+# Internal implementation
+
+
+_Format = namedtuple('Format', 'name description extensions')
+_Codec = namedtuple('_Codec', 'formats read write info')
+
+FORMATS = {n: _Format(n, d, x) for n, d, x in [
+    ('AAC', "Advanced Audio Coding", {'.aac'}),
+    ('AIFC', "Compressed AIFF", {'.aifc'}),
+    ('AIFF', "Audio IFF", {'.aiff'}),
+    ('AU', "Sun AU", {'.au'}),
+    ('AVI', "Audio Video Interlaced", {'.avi'}),
+    ('CAF', "Apple Core Audio Format", {'.caf'}),
+    ('FLAC', "FLAC", {'.flac'}),
+    ('M4A', "MPEG-4 Audio Lossless", {'.m4a', '.m4r'}),
+    ('M4B', "MPEG-4 Audio Books", {'.m4b'}),
+    ('MP2', "MPEG audio layer 2", {'.mp2'}),
+    ('MP3', "MPEG audio layer 3", {'.mp3'}),
+    ('MP4', "MPEG-4 Part 14", {'.mp4'}),
+    ('OGG', "Ogg", {'.ogg'}),
+    ('OGA', "Ogg Audio", {'.oga'}),
+    ('MOGG', "Multitrack Ogg", {'.mogg'}),
+    ('OPUS', "Ogg Opus", {'.opus'}),
+    ('WAV', "Waveform Audio", {'.wav'}),
+    ('WEBM', "WebM", {'.webm'}),
+    ('WMA', "Windows Media Audio", {'.wma'}),
+]}
+
+
+_formats = None
+_extensions = None
+def _init():
+    global _formats, _extensions
+    if not _formats:
+        _formats = dict()
+        # Attempt to instantiate each codec and query it. Some will fail,
+        # because they are not supported on the current platform.
+        for func in [_ffmpeg, _afconvert, _soundfile, _builtin]:
+            try:
+                codec = func()
+                _formats.update({f.name: codec for f in codec.formats()})
+            except:
+                pass
+    if not _extensions:
+        _extensions = dict()
+        # Build a table mapping file extensions to the codecs supporting them.
+        for fmtname, codec in _formats.iteritems():
+            fmt = FORMATS[fmtname]
+            for ext in FORMATS[fmtname].extensions:
+                _extensions[ext] = codec
+
+
+def _dispatch(file):
+    _init()
+    return _extensions[os.path.splitext(file)[1].lower()]
 
 
 def _fail_formats():
@@ -76,123 +144,137 @@ def _fail_info(file):
         raise FileNotFoundError(file)
 
 
-def _soundfile_formats():
-    # Get a list of the formats supported by the soundfile lib.
-    names = soundfile.available_formats().iterkeys()
-    return [FORMATS[n] for n in names if n in FORMATS]
+def _soundfile():
+    import soundfile
+
+    def _formats():
+        # Get a list of the formats supported by the soundfile lib.
+        names = soundfile.available_formats().iterkeys()
+        return [FORMATS[n] for n in names if n in FORMATS]
+
+    def _read(file):
+        data, samplerate = soundfile.read(file)
+        # soundfile returns [frames, channels] which is contrary to numpy
+        # conventions, so we'll transpose the output if necessary
+        return data.transpose(), samplerate
+
+    def _write(file, clip):
+        data, samplerate = clip
+        soundfile.write(file, data, samplerate)
+
+    def _info(file):
+        info = soundfile.info(file)
+        return Info(
+            name=info.name,
+            format=info.format,
+            dtype=info.subtype,
+            samplerate=info.samplerate,
+            channels=info.channels,
+            length=info.frames,
+            duration=info.frames / float(info.samplerate)
+        )
+
+    return _Codec(_formats, _read, _write, _info)
 
 
-def _soundfile_read(file):
-    data, samplerate = soundfile.read(file)
-    # soundfile returns [frames, channels] which is contrary to numpy
-    # conventions, so we'll transpose the output if necessary
-    return data.transpose(), samplerate
+def _ffmpeg():
+    # Make sure ffmpeg is present and responding.
+    _execpipe('ffmpeg', '-version')
 
-
-def _soundfile_write(file, clip):
-    data, samplerate = clip
-    soundfile.write(file, data, samplerate)
-
-
-def _soundfile_info(file):
-    info = soundfile.info(file)
-    return Info(
-        name=info.name,
-        format=info.format,
-        dtype=info.subtype,
-        samplerate=info.samplerate,
-        channels=info.channels,
-        length=info.frames,
-        duration=info.frames / float(info.samplerate)
-    )
-
-
-_Soundfile = _Codec(
-    _soundfile_formats, _soundfile_read, _soundfile_write, _soundfile_info)
-
-
-def _ffmpeg_formats():
-    output = _execpipe('ffmpeg', '-formats')
-    # The ffmpeg -formats output begins each line with some leading
-    # spaces, one or both of "D" or "E", and one or two more spaces.
-    # The format name always begins on column 4.
-    out = list()
-    for line in output.splitlines():
-        fields = line.split()
-        if len(fields) < 3:
-            continue
-        if not fields[0] in ['D', 'E', 'DE']:
-            continue
-        name = fields[1]
-        fmt = FORMATS.get(name.upper())
-        if fmt:
-            out.append(fmt)
-    return out
-
-
-def _ffmpeg_read(file):
-    try:
-        fd, temp = tempfile.mkstemp(suffix='.wav')
-        _execpipe('ffmpeg', '-v', '1', '-y', '-i', file,
-                '-vn', '-f', 'wav', temp)
-        return _wav_read(temp)
-    finally:
-        os.close(fd)
-        os.remove(temp)
-
-
-_Ffmpeg = _Codec(
-    _ffmpeg_formats, _ffmpeg_read, _fail_write, _fail_info)
-
-
-def _afconvert_formats():
-    apple_aliases = {
-        'adts': 'AAC', 'm4af': 'M4A', 'm4bf': 'M4B', 'caff': 'CAF',
-        'MPG1': 'MP1', 'MPG2': 'MP2', 'MPG3': 'MP3', 'mp4f': 'MP4',
-        'WAVE': 'WAV',
-    }
-
-    fmts = []
-    try:
-        output = _execpipe('afconvert', '--help-formats')
-    except:
-        output = ""
-        pass
-    # afconvert documentation here:
-    # https://ss64.com/osx/afconvert.html
-    # Example output:
-    # 'AIFF' = AIFF (.aiff, .aif)
-    #               data_formats: I8 BEI16 BEI24 BEI32
-    # The line begins with a four-char code in single quotes, then.
-    # This is Apple's version of the format name.
-    for line in output.splitlines():
-        line = line.strip()
-        if line[0] == "'" and line[5] == "'":
-            name = line[1:5]
-            name = Afconvert.apple_aliases.get(name, name)
-            fmt = FORMATS.get(name)
+    def _formats():
+        output = _execpipe('ffmpeg', '-formats')
+        # The ffmpeg -formats output begins each line with some leading
+        # spaces, one or both of "D" or "E", and one or two more spaces.
+        # The format name always begins on column 4.
+        out = list()
+        for line in output.splitlines():
+            fields = line.split()
+            if len(fields) < 3:
+                continue
+            if not fields[0] in ['D', 'E', 'DE']:
+                continue
+            name = fields[1]
+            fmt = FORMATS.get(name.upper())
             if fmt:
-                fmts.append(fmts)
-    return fmts
+                out.append(fmt)
+        return out
+
+    def _read(file):
+        try:
+            fd, temp = tempfile.mkstemp(suffix='.wav')
+            _execpipe('ffmpeg', '-v', '1', '-y', '-i', file,
+                    '-vn', '-f', 'wav', temp)
+            return _wav_read(temp)
+        finally:
+            os.close(fd)
+            os.remove(temp)
+
+    return _Codec(_formats, _read, _fail_write, _fail_info)
 
 
-def _afconvert_read(file):
-    # use ffmpeg to convert the input file to a temporary wav file we can read
-    try:
-        fd, temp = tempfile.mkstemp(suffix='.wav')
-        _execpipe('afconvert', '-d', 'LEI16', '-f', 'WAVE', file, temp)
-        return _wav_read(temp)
-    finally:
-        os.close(temp)
-        os.remove(temp)
+def _afconvert():
+    # Make sure afconvert will respond to a hello.
+    _execpipe('afconvert', '-h')
+
+    def _formats():
+        apple_aliases = {
+            'adts': 'AAC', 'm4af': 'M4A', 'm4bf': 'M4B', 'caff': 'CAF',
+            'MPG1': 'MP1', 'MPG2': 'MP2', 'MPG3': 'MP3', 'mp4f': 'MP4',
+            'WAVE': 'WAV',
+        }
+
+        fmts = []
+        try:
+            output = _execpipe('afconvert', '--help-formats')
+        except:
+            output = ""
+            pass
+        # afconvert documentation here:
+        # https://ss64.com/osx/afconvert.html
+        # Example output:
+        # 'AIFF' = AIFF (.aiff, .aif)
+        #               data_formats: I8 BEI16 BEI24 BEI32
+        # The line begins with a four-char code in single quotes, then.
+        # This is Apple's version of the format name.
+        for line in output.splitlines():
+            line = line.strip()
+            if line[0] == "'" and line[5] == "'":
+                name = line[1:5]
+                name = apple_aliases.get(name, name)
+                fmt = FORMATS.get(name)
+                if fmt:
+                    fmts.append(fmts)
+        return fmts
+
+    def _read(file):
+        # convert the input file to a temporary wav file we can read
+        try:
+            fd, temp = tempfile.mkstemp(suffix='.wav')
+            _execpipe('afconvert', '-d', 'LEI16', '-f', 'WAVE', file, temp)
+            return _wav_read(temp)
+        finally:
+            os.close(temp)
+            os.remove(temp)
+
+    return _Codec(_formats, _read, _fail_write, _fail_info)
 
 
-_Afconvert = _Codec(
-    _afconvert_formats, _afconvert_read, _fail_write, _fail_info)
+def _builtin():
+
+    def _formats():
+        return [FORMATS['WAV']]
+
+    return _Codec(_formats, _wav_read, _fail_write, _fail_info)
 
 
-def _wav_formats():
-    return [FORMATS['WAV']]
+def _execpipe(*args):
+    pipe = subprocess.PIPE
+    proc = subprocess.Popen(args, stdin=pipe, stdout=pipe, stderr=pipe)
+    output, _ = proc.communicate()
+    retcode = proc.returncode
+    if retcode:
+        raise subprocess.CalledProcessError(retcode, args, output=output)
+    return output
 
 
 def _wav_read(file):
@@ -240,76 +322,4 @@ def _wav_read(file):
     return data, samplerate
 
 
-_Wav = _Codec(_wav_formats, _wav_read, _fail_write, _fail_info)
-
-
-_formats = None
-_extensions = None
-def _init():
-    global _formats, _extensions
-    if not _formats:
-        _formats = dict()
-        # Attempt to instantiate each codec and query it. Some will fail,
-        # because they are not supported on the current platform.
-        for codec in [_Ffmpeg, _Afconvert, _Soundfile, _Wav]:
-            try:
-                _formats.update({f.name: codec for f in codec.formats()})
-            except:
-                pass
-    if not _extensions:
-        _extensions = dict()
-        # Build a table mapping file extensions to the codecs supporting them.
-        for fmtname, codec in _formats.iteritems():
-            fmt = FORMATS[fmtname]
-            for ext in FORMATS[fmtname].extensions:
-                _extensions[ext] = codec
-
-
-def _dispatch(file):
-    _init()
-    return _extensions[os.path.splitext(file)[1].lower()]
-
-
-def formats():
-    _init()
-    global _formats
-    return _formats.keys()
-
-
-def extensions():
-    _init()
-    global _extensions
-    return _extensions.keys()
-
-
-def read(file):
-    """Retrieve audio samples from a file.
-
-    We will use the 'soundfile' module if it supports the file's format.
-    Otherwise, we will attempt to decode the file using 'ffmpeg' or 'afconvert'.
-
-    Parameters
-    ----------
-    file : str
-        The file to read from.
-
-    Returns
-    -------
-    data : numpy.ndarray
-        If the file is monaural, a one-dimensional array of samples; otherwise
-        a two-dimensional array of [channels, samples].
-
-    samplerate : int
-        The sampling frequency of the audio data in Hz.
-    """
-
-    return Clip(*_dispatch(file).read(file))
-
-
-def write(file, clip):
-    _dispatch(file).write(file, clip)
-
-
-def info(file):
-    return _dispatch(file).info(file)
 
