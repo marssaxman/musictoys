73ff8650305b314b750a4474bf144149e64a78a2
Mars Saxman <mars@redecho.org>
2018-05-15 12:56:31 -0700

    moved the HSL-to-RGB vector convertion code into the scrapbook

--- notebooks/scrapbook/plot.py
+++ notebooks/scrapbook/plot.py
@@ -66,3 +66,51 @@ def colorgram(matrix, x=None, y=None):
     plt.imshow(np.swapaxes(matrix, 0, 1))
     plt.show()
 
+
+def hsl_to_rgb(h, s, l):
+    # vectorized convertion of hue, saturation, lightness planes into
+    # corresponding red, green, blue planes
+    shape = h.shape
+
+    # all inputs and outputs range 0..1
+    r = np.zeros(shape)
+    g = np.zeros(shape)
+    b = np.zeros(shape)
+
+    # where the color is totally desaturated, use only luminance
+    grey = (s==0)
+    r[grey] = l[grey]
+    g[grey] = l[grey]
+    b[grey] = l[grey]
+
+    # scale the saturation differently around medium luminance
+    q = np.zeros(shape)
+    low_luma = l < 0.5
+    q[low_luma] = l[low_luma] * (1 + s[low_luma])
+    hi_luma = l >= 0.5
+    q[hi_luma] = l[hi_luma] + s[hi_luma] - l[hi_luma] * s[hi_luma]
+    # the other hue factor is proportional
+    p = 2 * l - q;
+
+    def channel(t):
+        # enforce limits
+        t[t < 0] += 1.0
+        t[t > 1] -= 1.0
+        x = np.zeros(shape)
+        tA = t < 1/6.
+        x[tA] = p[tA] + (q[tA] - p[tA]) * 6 * t[tA]
+        tB = (t >= 1/6.) & (t < 1/2.)
+        x[tB] = q[tB]
+        tC = (t >= 1/2.) & (t < 2/3.)
+        x[tC] = p[tC] + (q[tC] - p[tC]) * (2/3. - t[tC]) * 6
+        tD = t >= 2/3.
+        x[tD] = p[tD]
+        return x
+
+    chroma = (s != 0)
+    r[chroma] = channel(h + 1./3.)[chroma]
+    g[chroma] = channel(h)[chroma]
+    b[chroma] = channel(h - 1./3.)[chroma]
+
+    return r, g, b
+
